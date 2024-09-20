9ee30c732ce8d5b572fdbdc6f2467f41e0f82520
possumlabscrypto <alex@possumlabs.io>
2024-09-20 16:27:37 +0200

    Add Deploy & Verify script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.19;
+
+import {Script, console} from "lib/forge-std/src/Script.sol";
+import {PossumCore} from "src/PossumCore.sol";
+
+contract Deploy is Script {
+    function setUp() public {}
+
+    function run() public returns (address coreAddress) {
+        vm.startBroadcast();
+
+        // Configure optimizer settings
+        vm.store(address(this), bytes32("optimizer"), bytes32("true"));
+        vm.store(address(this), bytes32("optimizerRuns"), bytes32(uint256(1000)));
+
+        PossumCore possumCore = new PossumCore();
+        coreAddress = address(possumCore);
+
+        vm.stopBroadcast();
+    }
+}
+
+// forge script script/Deploy.s.sol:Deploy --rpc-url $ARB_MAINNET_URL --private-key $PRIVATE_KEY --broadcast --verify --verifier etherscan --etherscan-api-key $ARBISCAN_API_KEY --optimize --optimizer-runs 1000
